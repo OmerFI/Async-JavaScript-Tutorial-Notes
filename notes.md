59d13c9aa061459cdc8ee1acbef576119e1e7769
Omer Furkan Isleyen <omergumushane@hotmail.com>
2021-10-07 00:45:27 +0300

    Add video 11 changes and notes

--- notes.md
+++ notes.md
@@ -337,3 +337,31 @@ console.log(4);
 // const test = getTodos();
 // console.log(test); // Promise
 ```
+
+## Video 11 - Throwing Errors
+
+```js
+// fetch api - Async & Await - Throwing Errors
+
+const getTodos = async () => {
+
+    const response = await fetch('todos/luigis.json')
+
+    if (response.status !== 200) {
+        throw new Error('cannot fetch the data');
+        // when we throw a new error inside an asynchronous function the promise returned by this asynchronous function is rejected.
+        // therefore if it's rejected we're going to catch it in the catch function
+    }
+    const data = await response.json();
+    return data;
+
+};
+// whenever we call an asynchronous function that always returns a promise
+
+getTodos()
+    .then(data => console.log('resolved:', data))
+    .catch(err => console.log('rejected:',err.message));
+
+// const test = getTodos();
+// console.log(test); // Promise
+```
--- sandbox.js
+++ sandbox.js
@@ -1,21 +1,23 @@
-// fetch api - Async & Await
+// fetch api - Async & Await - Throwing Errors
 
 const getTodos = async () => {
 
-    const response = await fetch('todos/luigi.json')
+    const response = await fetch('todos/luigis.json')
+
+    if (response.status !== 200) {
+        throw new Error('cannot fetch the data');
+        // when we throw a new error inside an asynchronous function the promise returned by this asynchronous function is rejected.
+        // therefore if it's rejected we're going to catch it in the catch function
+    }
     const data = await response.json();
     return data;
 
 };
 // whenever we call an asynchronous function that always returns a promise
 
-console.log(1);
-console.log(2);
 getTodos()
     .then(data => console.log('resolved:', data))
-    .catch(err => console.log(err));
-console.log(3);
-console.log(4);
+    .catch(err => console.log('rejected:',err.message));
 
 // const test = getTodos();
 // console.log(test); // Promise
