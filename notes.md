32e9620e865da69e065e257c934c937e8431173e
Omer Furkan Isleyen <omergumushane@hotmail.com>
2021-10-06 23:55:16 +0300

    Add video 9 changes and notes

--- notes.md
+++ notes.md
@@ -294,3 +294,20 @@ getTodos('todos/luigi.json').then(data => {
     console.log('promise rejected:', err);
 });
 ```
+
+## Video 9 - The Fetch API
+
+```js
+// fetch api
+
+fetch('todos/luigi.json').then((response) => {
+    console.log('resolved', response);
+    return response.json();
+}).then(data => {
+    console.log(data);
+}).catch((err) => {
+    console.log('rejected', err);
+});
+// yanlış endpointe istek atsak bile catch çalışmayacak
+// the way the fetch api works is that the promise is only ever rejected when we get some kind of network error.
+```
--- sandbox.js
+++ sandbox.js
@@ -1,32 +1,12 @@
-const getTodos = (resource) => {
+// fetch api
 
-    return new Promise((resolve, reject) => {
-        const request = new XMLHttpRequest(); // this creates us a request object
-
-        request.addEventListener('readystatechange', function () { // this fires every time there's a state change
-            //console.log(request, request.readyState);
-            if (request.readyState === 4 && request.status === 200) {
-                const data = JSON.parse(request.responseText);
-                resolve(data);
-            } else if (request.readyState === 4) {
-                reject('error getting resource');
-            }
-        });
-
-        // request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
-        request.open('GET', resource)
-        request.send()
-    });
-};
-
-getTodos('todos/luigi.json').then(data => {
-    console.log('promise 1 resolved:', data);
-    return getTodos('todos/mario.json');
+fetch('todos/luigi.json').then((response) => {
+    console.log('resolved', response);
+    return response.json();
 }).then(data => {
-    console.log('promise 2 resolved:', data);
-    return getTodos('todos/shauns.json');
-}).then(data => {
-    console.log('promise 3 resolved:', data);
-}).catch(err => {
-    console.log('promise rejected:', err);
+    console.log(data);
+}).catch((err) => {
+    console.log('rejected', err);
 });
+// yanlış endpointe istek atsak bile catch çalışmayacak
+// the way the fetch api works is that the promise is only ever rejected when we get some kind of network error.
